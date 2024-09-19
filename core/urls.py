2ba6b5e8b1090177f18906b72fc1b581d455542e
yoldashali muxammadkarim <m2220134@gmail.com>
2024-09-19 11:06:01 +0500

    swagger

--- core/urls.py
+++ core/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
 
     path('schema/', user_passes_test(is_supperuser)(SpectacularAPIView.as_view()), name='schema'),
     path('swagger/', user_passes_test(is_supperuser)(SpectacularSwaggerView.as_view()), name='swagger-ui'),
-    path('redoc/', user_passes_test(is_supperuser)(SpectacularRedocView.as_view()), name='redoc  ')
+    path('redoc/', user_passes_test(is_supperuser)(SpectacularRedocView.as_view()), name='redoc')
 ]
 
 if settings.DEBUG:
